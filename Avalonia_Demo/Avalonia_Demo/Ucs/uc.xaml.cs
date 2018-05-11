100d7bbc7d2c1ef8e0f95a4492c96da3b95c7620
zhanghongwei <hw_zhang@leigod.cn>
2018-05-11 11:18:37 +0800

    master

---
+++ Avalonia_Demo/Avalonia_Demo/Ucs/uc.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Avalonia_Demo.Ucs
+{
+    public class uc : UserControl
+    {
+        public uc()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
